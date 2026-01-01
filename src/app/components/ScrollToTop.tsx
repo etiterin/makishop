dd4b0a35ff92bd342cb424dbf02d3cb66a3a1ee0
etiterin <titerin.evgeny@gmail.com>
2026-01-02 02:04:38 +0300

    try fix scroll

--- src/app/components/ScrollToTop.tsx
+++ src/app/components/ScrollToTop.tsx
@@ -1,10 +1,16 @@
-import { useEffect } from 'react';
+import { useLayoutEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
 export function ScrollToTop() {
   const { pathname } = useLocation();
 
-  useEffect(() => {
+  useLayoutEffect(() => {
+    // Отключаем автоматическое восстановление скролла браузером,
+    // чтобы оно не мешало нашему принудительному скроллу вверх.
+    if ('scrollRestoration' in window.history) {
+      window.history.scrollRestoration = 'manual';
+    }
+
     window.scrollTo(0, 0);
   }, [pathname]);
 
