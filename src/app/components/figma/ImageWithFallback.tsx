437c77f7b946e4efe686627171171cc09b005cdd
etiterin <titerin.evgeny@gmail.com>
2026-01-02 01:07:00 +0300

    fix images

--- src/app/components/figma/ImageWithFallback.tsx
+++ src/app/components/figma/ImageWithFallback.tsx
@@ -22,6 +22,13 @@ export function ImageWithFallback(props: React.ImgHTMLAttributes<HTMLImageElemen
       </div>
     </div>
   ) : (
-    <img src={src} alt={alt} className={className} style={style} {...rest} onError={handleError} />
+    <img 
+      src={src} 
+      alt={alt} 
+      className={`bg-muted/30 ${className}`} // Оставил только фон, убрал анимацию opacity
+      style={style} 
+      {...rest} 
+      onError={handleError} 
+    />
   )
 }
