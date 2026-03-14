3967c57956de2d09453d727ea36759ae4e79b963
etiterin <titerin.evgeny@gmail.com>
2026-03-14 16:00:59 +0300

    fixed back button

--- src/app/components/ScrollToTop.tsx
+++ src/app/components/ScrollToTop.tsx
@@ -5,12 +5,6 @@ export function ScrollToTop() {
   const { pathname } = useLocation();
 
   useLayoutEffect(() => {
-    // Отключаем автоматическое восстановление скролла браузером,
-    // чтобы оно не мешало нашему принудительному скроллу вверх.
-    if ('scrollRestoration' in window.history) {
-      window.history.scrollRestoration = 'manual';
-    }
-
     window.scrollTo(0, 0);
   }, [pathname]);
 
