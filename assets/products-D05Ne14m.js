e6a543290b54d0a7eea286f637ad570ae609a561
etiterin <titerin.evgeny@gmail.com>
2026-03-28 20:36:44 +0300

    Updates

---
+++ assets/products-D05Ne14m.js
@@ -0,0 +1,45 @@
+import{c as e}from"./index-VE42ZYqr.js";/**
+ * @license lucide-react v0.487.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const a=[["path",{d:"m15 18-6-6 6-6",key:"1wnfg3"}]],o=e("chevron-left",a);/**
+ * @license lucide-react v0.487.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const i=[["path",{d:"m9 18 6-6-6-6",key:"mthhwq"}]],g=e("chevron-right",i),s=[{id:1,name:"Голографические стикеры Вапореон",price:300,category:"sticker",fandoms:["Pokemon"],images:["/images/uploads/vap1.webp"],description:"Окунись в сияющий водоворот магии с этим набором голографических наклеек, посвящённых Вапореону! Качественная плёнка с радужным переливом. Формат А6, плоттерная резка.",inStock:!0},{fandoms:["Animals","Pokemon"],price:400,name:"Гача Ивилюции Рамуне",inStock:!0,detailsTitle:"Рамуне-ивилюции - случайный брелочек",images:["/images/uploads/tyhs.jpg","/images/uploads/sr6hraztf.jpg","/images/uploads/ndxytjs.jpg","/images/uploads/rhgrthrjsry.jpg"],id:13,description:`Гача — это формат сюрприза: вы не знаете, какой именно брелок попадётся, пока не откроете капсулу. Это делает покупку интересной и немного азартной.
+
+В этой серии — 10 разных брелоков с ивилюциями и напитками рамуне. Каждый дизайн уникален и выполнен с вниманием к деталям.
+
+Если вам понравится конкретный брелок, его можно выкупить отдельно — доступны все варианты, кроме одного редкого на блестящем акриле.`,category:"keychain",detailsItems:["Размер брелочка 3,5 см","На цветной веревочке-карабине","10 вариантов ивилюций","Есть секретная опция"]},{fandoms:["Animals","Pokemon"],price:450,name:"Гача Ивилюции Рамуне [Выкуп]",inStock:!0,detailsTitle:"Выкуп Рамуне Гачи. ",images:["/images/uploads/ерфенкр.jpg","/images/uploads/line-evdrnx11.jpg","/images/uploads/line-evdrnx112.jpg","/images/uploads/line-evdrnx1144.jpg","/images/uploads/gzhryhehj.jpg","/images/uploads/gluiolf.jpg"],id:14,description:`Эта страничка товара для выбора выкупа ивика из гачи с Рамуне.
+
+Добавьте этот товар в корзину и при оформлении напишите в описании номер желаемой опции!
+
+Все опции доступны к выкупу КРОМЕ Шайни Иви.
+
+В этой серии — 10 разных брелоков с ивилюциями и напитками рамуне. Каждый дизайн уникален и выполнен с вниманием к деталям.
+
+Если вам понравится конкретный брелок, его можно выкупить отдельно — доступны все варианты, кроме одного редкого на блестящем акриле.
+
+1 - Иви 🧡
+
+2 - Умбреон 🖤
+
+3 - Флареон ❤️‍🔥
+
+4 - Жолтеон 💛
+
+5 - Лифеон 💚
+
+6 - Вапореон 💙
+
+7 - Гласеон 🤍
+
+8 - Эспеон 💜
+
+9 - Сильвеон 🩷
+
+Шайни Иви доступен только в Гаче!`,category:"keychain",detailsItems:["Пожалуйста, добавьте товар в корзину и напишите в описании к заказу номер желаемой позиции.","Номера представлены в описании и на картинке в карусели."]},{inStock:!0,id:2,images:["/images/uploads/gacha.webp"],category:"keychain",name:'Гача-брелок "Восточные Символы"',price:500,fandoms:["Other"],description:"Испытай удачу и узнай какой из шести Восточных покровителей присоединится к твоему путешествию."},{inStock:!0,id:3,name:"Стикерпак A6 Сампо Коски",price:250,category:"sticker",fandoms:["Honkai Star Rail"],images:["/images/uploads/sampo.webp"]},{inStock:!1,images:["/images/uploads/nan.webp"],id:4,name:"Брелок на сияющем акриле Наначи",price:700,category:"keychain",fandoms:["Made in Abyss"]},{inStock:!0,id:5,name:"Брелок Silent Hill Лис",price:650,category:"keychain",fandoms:["Creepy"],images:["/images/uploads/lis.webp"]},{inStock:!0,id:21,name:"Брелок Хинако",price:650,category:"keychain",fandoms:["Creepy"],images:["/images/uploads/hinakofuhfhv.jpg","/images/uploads/hinakofuufnv.jpg"]},{inStock:!0,id:20,name:"Стикерлайн Сайлент Хилл",price:200,category:"sticker",fandoms:["Creepy"],images:["/images/uploads/shffijvnyjgdo.jpg","/images/uploads/shfovobjh.jpg"],description:"Вытянутый нестандартный формат стикеров по мотивам хоррор-игры"},{inStock:!0,id:6,name:"Лента Шелковая Сейлор Мун",price:550,category:"ribbon",fandoms:["Fantasy"],images:["/images/uploads/sail.webp"]},{inStock:!0,id:7,name:"Стикеры Спирит",price:200,category:"sticker",fandoms:["Animals"],images:["/images/uploads/img_2187.webp","/images/uploads/img_2188.webp"]},{inStock:!0,id:8,name:"Стикеры Мелоди",price:200,category:"sticker",fandoms:["Original","Fantasy","Nature"],images:["/images/uploads/img_2195.webp","/images/uploads/img_2196.webp"]},{inStock:!0,id:9,name:"Стикеры Нулевые",price:300,category:"sticker",fandoms:["Original","Animals"],images:["/images/uploads/img_2190.webp","/images/uploads/img_2189.webp"]},{inStock:!0,id:10,name:"Стикеры Монолог Фармацевта",price:200,category:"sticker",fandoms:["Monologue Apothecary"],images:["/images/uploads/img_2194.webp","/images/uploads/img_2193.webp"]},{inStock:!0,id:11,images:["/images/uploads/img_2201.webp","/images/uploads/img_2202.webp"],name:"Стикеры Снежные Эльфы",price:150,category:"sticker",fandoms:["Original","Fantasy"]},{inStock:!0,id:12,name:"Обложка на паспорт Гранатовая Ведьма",price:500,category:"textile",fandoms:["Original","Creepy","Fantasy"],images:["/images/uploads/img_2192.webp","/images/uploads/img_2191.webp"]},{inStock:!0,id:15,name:"Значок Сердечко Фапута",price:400,category:"badge",fandoms:["Made in Abyss"],images:["/images/uploads/fapuhdhdbd.jpg"],description:"Значок с покрытием Софт-тач"},{inStock:!0,id:16,name:"Значок Сердечко Наначи",price:400,category:"badge",fandoms:["Made in Abyss"],images:["/images/uploads/nanajfjfjfjvjf.jpg"],description:"Значок с покрытием Софт-тач"},{inStock:!0,id:17,name:"Стикерпак А6 Лягушки",price:200,category:"sticker",fandoms:["Animals"],images:["/images/uploads/lagydhfhhf.jpg","/images/uploads/lagushfhvvnughvh.jpg"],description:"А6 стикерпак на виниловой пленке"},{inStock:!0,id:18,name:"Золотой стикерпак Котики Бастет",price:300,category:"sticker",fandoms:["Animals","Fantasy"],images:["/images/uploads/bastgklggogo.jpg","/images/uploads/basturhgventhrhgvrue.jpg"],description:`Сияющий золотой стикерпак по мотивам Египетской мифологии 
+
+Котики и золото сочетаются идеально`},{inStock:!0,id:19,name:"Стикерпак Летняя Япония",price:200,category:"sticker",images:["/images/uploads/kitsfhhvjfnrhtj.jpg","/images/uploads/kitshfhhv.jpg"],description:"Стикерпак А6 с сахарной ламинацией"},{fandoms:["Monologue Apothecary"],price:550,name:"Брелок Мао-Мао",inStock:!0,detailsTitle:"Покрыт прочной эпоксидной смолой",images:["/images/uploads/mmaofthbfoopjh.jpg","/images/uploads/mmaooydgyfhfnfk.jpg"],id:22,description:"Брелок покрыт защитным слоем эпоксидной смолы и украшен блестками. Гарантирует более долгий срок ношения и сохраняет принт. Места расположения блесток отличаются от изделия к изделию. Возможны неровности поверхности не влияющие на прочность.",category:"keychain",detailsItems:["Размер 6 сантиметров","Покрыт прочным слоем эпоксидной смолы","Подвеска шелковистая кисточка"]},{fandoms:["Monologue Apothecary"],price:550,name:"Брелок Джинши",inStock:!0,detailsTitle:"Покрыт прочной эпоксидной смолой",images:["/images/uploads/jinshgfyfyfjmgkc.jpg","/images/uploads/jinshgfyfikguvi.jpg"],id:23,description:"Брелок покрыт защитным слоем эпоксидной смолы и украшен блестками. Гарантирует более долгий срок ношения и сохраняет принт. Места расположения блесток отличаются от изделия к изделию. Возможны неровности поверхности не влияющие на прочность.",category:"keychain",detailsItems:["Размер 6 сантиметров","Покрыт прочным слоем эпоксидной смолы","Подвеска шелковистая кисточка"]}],t={products:s};export{o as C,g as a,t as p};
